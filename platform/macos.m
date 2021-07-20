08d01439f603b1ed2c29e83e0968c9d044010090
j$ <jds@60.fov>
2021-07-20 00:41:35 -0500

    fixed macos drawing
    fixed macos platform drawing code also had a little fun with gradients and interp functions

--- platform/macos.m
+++ platform/macos.m
@@ -1,25 +1,28 @@
 #import <stdlib.h>
 #import <stdio.h>
+#import <string.h>
+#import <assert.h>
 #import <mach/mach_time.h>
 #import <Cocoa/Cocoa.h>
 
-struct platform_win {
+typedef struct window window_t;
+typedef struct surface surface_t;
+
+struct surface {
+	int width;
+	int height;
+	unsigned char* buffer;
+};
+struct window {
+    int should_close;
+	surface_t* surface;
+
     NSWindow* handle;
 };
 
-typedef struct platform_win win_t;
-
-struct window {
-    int should_close;
-
-    win_t data;
-};
-
-typedef struct window window_t;
-
 static double time_init = 0;
 static double time_freq = -1;
-NSAutoreleasePool* pool;
+static NSAutoreleasePool* pool;
 
 @interface WindowDelegate : NSObject <NSWindowDelegate>
 @end
@@ -39,6 +42,51 @@ NSAutoreleasePool* pool;
     return NO;
 }
 @end
+
+@interface ContentView : NSView
+@end
+
+@implementation ContentView {
+	window_t* _window;
+}
+
+- (instancetype) initWithWindow: (window_t*) window {
+    self = [super init];
+    _window = window;
+    return self;
+}
+
+- (BOOL) acceptsFirstResponder {
+	return YES;
+}
+
+- (void) drawRect: (NSRect) dirtyRect {
+	surface_t* surf = _window->surface;
+	unsigned char* buff = surf->buffer;
+	int pc = surf->width * surf->height;
+	for(int i = 0; i < pc; i++) {
+		double c = (double) i / (double) pc * 255;
+	}
+	NSBitmapImageRep* rep = [[[NSBitmapImageRep alloc] 
+		initWithBitmapDataPlanes:&buff
+					  pixelsWide:surf->width
+					  pixelsHigh:surf->height
+				   bitsPerSample:8
+				 samplesPerPixel:3
+						hasAlpha:NO
+						isPlanar:NO
+				  colorSpaceName:NSCalibratedRGBColorSpace
+					 bytesPerRow:surf->width*4
+					bitsPerPixel:32] autorelease];
+
+	NSImage* image = [[[NSImage alloc] init] autorelease];
+	assert(image != nil);
+	[image addRepresentation:rep];
+	[image drawInRect:dirtyRect];
+}
+@end
+
+
 
 double platform_cpu_time() {
     return mach_absolute_time() * time_freq;
@@ -63,6 +111,7 @@ void platform_start() {
     pool = [[NSAutoreleasePool alloc] init];
 
     [NSApplication sharedApplication];
+	[NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
     [NSApp finishLaunching];
 }
 
@@ -85,12 +134,24 @@ void poll() {
 }
 
 window_t* window_create(int width, int height) {
+	assert(NSApp && width > 0 && height > 0);
     window_t* window;
-    
+    surface_t* surface;
+	
     window = (window_t*) malloc(sizeof(window_t));
+	memset(window, 0, sizeof(window_t));
 
+	surface = (surface_t*) malloc(sizeof(surface_t));
+	surface->width = width;
+	surface->height = height;
+	int buf_size = 4 * width * height;
+	surface->buffer = (unsigned char*) malloc(buf_size);
+	memset(surface->buffer, 255, buf_size); 
+	window->surface = surface;
+	
     NSWindow* handle;
     WindowDelegate* delegate;
+	ContentView* view;
     NSRect rect = NSMakeRect(0, 0, width, height);
     NSUInteger style =  NSWindowStyleMaskTitled |
                         NSWindowStyleMaskMiniaturizable |
@@ -101,28 +162,47 @@ window_t* window_create(int width, int height) {
                                            backing:NSBackingStoreBuffered
                                              defer:NO];
 
+	assert(handle != nil);
     // [handle setTitle:[NSString stringWithUTF8String:title];
-
+	[handle setColorSpace:[NSColorSpace genericRGBColorSpace]];
+    window->handle = handle;
+	
     delegate = [[WindowDelegate alloc] initWithWindow:window];
+	assert(delegate != nil);
     [handle setDelegate:delegate];
+
+	view = [[[ContentView alloc] initWithWindow:window] autorelease];
+	assert(view != nil);
+	[handle setContentView:view];
+	[handle makeFirstResponder:view];
+
     [handle center];
     [handle makeKeyAndOrderFront:nil];
-
-    window->data.handle = handle;
     return window;
 }
 
 void window_destroy(window_t* window) {
-    [window->data.handle orderOut: nil];
-    [[window->data.handle delegate] release];
-    [window->data.handle close];
+    [window->handle orderOut: nil];
+    [[window->handle delegate] release];
+    [window->handle close];
+	
+	free(window->surface->buffer);
+	free(window->surface);
     
     [pool drain];
     pool = [[NSAutoreleasePool alloc] init];
 }
 
+void window_present(window_t* window) {
+	[[window->handle contentView] setNeedsDisplay:YES];
+}
+
 int window_should_close(window_t* window) {
     return window->should_close;
+}
+
+surface_t* window_surface(window_t* window) {
+	return window->surface;
 }
 
 /*
--- platform/platform.nim
+++ platform/platform.nim
@@ -2,7 +2,16 @@
 
 
 type
-  Window* {.importc: "struct window_t", incompleteStruct} = object
+  Window* = object
+    should_close: cint
+    surface: ptr Surface
+
+    handle: pointer
+    
+  Surface* = object
+    width: cint
+    height: cint
+    buffer*: ptr uint8
 
 proc platform_start*() {.importc.}
 proc platform_end*() {.importc.}
@@ -12,12 +21,16 @@ proc platform_freq*(): float64 {.importc.}
 proc poll*() {.importc.}
 proc window_create*(width, height: cint): ptr Window {.importc.}
 proc window_destroy*(window: ptr Window) {.importc.}
-proc window_should_close*(window: ptr Window): int {.importc.}
+proc window_present*(window: ptr Window) {.importc.}
+proc window_surface*(window: ptr Window): ptr Surface {.importc.}
+proc window_should_close*(window: ptr Window): cint {.importc.}
 
 when isMainModule:
   platform_start()
   echo platform_time()
-  var window = window_create(600, 400);
+  var window = window_create(600, 400)
+  
+  echo window_should_close(window)
 
   while window_should_close(window) == 0:
     poll()
