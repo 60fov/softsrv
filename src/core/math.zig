db37886bcbf11538a3691628a633a04944be1d81
jds <tofu.local>
2024-11-02 16:51:43 -0400

    mat3

--- src/core/math.zig
+++ src/core/math.zig
@@ -137,11 +137,11 @@ pub const Vector = struct {
     }
 };
 
-const Mat3 = @Vector(9, f32);
+pub const Mat3 = @Vector(9, f32);
 
 // TODO support for not 3x3 matrices
-const Mat = struct {
-    fn identity() Mat3 {
+pub const Mat = struct {
+    pub fn identity() Mat3 {
         return .{
             1, 0, 0,
             0, 1, 0,
@@ -149,7 +149,7 @@ const Mat = struct {
         };
     }
 
-    fn scaling(sx: f32, sy: f32) Mat3 {
+    pub fn scaling(sx: f32, sy: f32) Mat3 {
         var result: Mat3 = @splat(0);
         result[0] = sx;
         result[4] = sy;
@@ -157,7 +157,7 @@ const Mat = struct {
         return result;
     }
 
-    fn translation(tx: f32, ty: f32) Mat3 {
+    pub fn translation(tx: f32, ty: f32) Mat3 {
         var result: Mat3 = @splat(0);
         result[0] = 1;
         result[2] = tx;
@@ -167,7 +167,7 @@ const Mat = struct {
         return result;
     }
 
-    fn rotation(theta: f32) Mat3 {
+    pub fn rotation(theta: f32) Mat3 {
         var result: Mat3 = @splat(0);
         result[0] = @cos(theta);
         result[1] = -@sin(theta);
@@ -177,7 +177,7 @@ const Mat = struct {
         return result;
     }
 
-    fn mul(a: Mat3, b: Mat3) Mat3 {
+    pub fn mul(a: Mat3, b: Mat3) Mat3 {
         var result: Mat3 = @splat(0);
         for (0..3) |i| {
             for (0..3) |j| {
@@ -189,7 +189,7 @@ const Mat = struct {
         return result;
     }
 
-    fn mulVec(m: Mat3, v: @Vector(2, f32)) @Vector(2, f32) {
+    pub fn mulVec(m: Mat3, v: @Vector(2, f32)) @Vector(2, f32) {
         return .{
             m[0] * v[0] + m[1] * v[1] + m[2],
             m[3] * v[0] + m[4] * v[1] + m[5],
