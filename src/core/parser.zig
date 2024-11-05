5498d5a17c224904cd0d8157ddffed7beb41545c
jds <tofu.local>
2024-11-05 02:41:19 -0500

    WaveFrontObj v0

--- src/core/parser.zig
+++ src/core/parser.zig
@@ -1,60 +1,47 @@
 const std = @import("std");
 const FixedBufferList = @import("list.zig").FixedBufferList;
 const SliceReader = @import("io.zig").SliceReader;
-
-// TODO
-// tokenize function
-// parse function
-// wavefrontobj data structure
+const megabytes = @import("mem.zig").megabytes;
+const gigabytes = @import("mem.zig").gigabytes;
 
 const WavefrontObj = struct {
-    const TokenKind = enum(u8) {
-        string,
-        newline,
+    // techinally should be 4, xyz[w]
+    vertex_list: [][3]f32,
 
-        hash_sym,
-        slash_sym,
-        bracket_open_sym,
-        bracket_close_sym,
-        v_sym,
-        vn_sym,
-        vt_sym,
-        vp_sym,
-        f_sym,
-    };
-
-    const Token = struct {
-        value: []const u8,
-        kind: TokenKind,
-    };
+    pub fn free(obj: *WavefrontObj, allocator: std.mem.Allocator) void {
+        allocator.free(obj.vertex_list);
+        obj.* = undefined;
+    }
 };
 
-fn parseObj(allocator: std.mem.Allocator) !void {
-    const file_allocator = std.heap.page_allocator;
-    const max_file_size = 1024 * 1024 * 1024;
-    const working_size = 1024 * 1024 * 32;
-    const scratch_size = 1024;
-    const scratch_buffer: []u8 = try allocator.alloc(u8, scratch_size);
-    defer allocator.free(scratch_buffer);
-    const working_buffer: []u8 = try allocator.alloc(u8, working_size);
-    defer allocator.free(working_buffer);
-    var file_buffer: []u8 = undefined;
-    // var scratch_fba = std.heap.FixedBufferAllocator.init(scratch_buffer);
-    var arena_fba = std.heap.FixedBufferAllocator.init(working_buffer);
-    var arena = std.heap.ArenaAllocator.init(arena_fba.allocator());
+const WavefrontObjLexicalTokenKind = enum(u8) {
+    string,
+    newline,
 
-    // kinda wanna play with mmap
-    const path = try std.fs.cwd().realpath("assets/models/Wolfen_2/Wolfen2.obj", scratch_buffer);
-    const file = try std.fs.openFileAbsolute(path, .{});
-    const reader = file.reader();
-    // TODO is reading file into memory meaningful here? (lol i dunno what i meant by this)
-    file_buffer = try reader.readAllAlloc(file_allocator, max_file_size);
-    var token_list = std.ArrayList(WavefrontObj.Token).init(arena.allocator());
+    hash_sym,
+    slash_sym,
+    bracket_open_sym,
+    bracket_close_sym,
+    v_sym,
+    vn_sym,
+    vt_sym,
+    vp_sym,
+    f_sym,
+};
+
+const WavefrontObjLexicalToken = struct {
+    value: []const u8,
+    kind: WavefrontObjLexicalTokenKind,
+};
+
+fn lexBuffer(allocator: std.mem.Allocator, buf: []const u8) ![]WavefrontObjLexicalToken {
+    var token_list = std.ArrayList(WavefrontObjLexicalToken).init(allocator);
     defer token_list.deinit();
 
-    var token_iter = std.mem.tokenizeAny(u8, file_buffer, " \r\n");
+    var token_iter = std.mem.tokenizeAny(u8, buf, " \r\n");
+
     while (token_iter.next()) |token_entry| {
-        var token_kind: WavefrontObj.TokenKind = undefined;
+        var token_kind: WavefrontObjLexicalTokenKind = undefined;
         // std.debug.print("token_entry \"{s}\"\n", .{token_entry});
         if (std.mem.eql(u8, token_entry, "#")) {
             token_kind = .hash_sym;
@@ -79,28 +66,12 @@ fn parseObj(allocator: std.mem.Allocator) !void {
         } else {
             token_kind = .string;
         }
-        const token = WavefrontObj.Token{
-            // TODO copy?
+        const token = WavefrontObjLexicalToken{
+            // TODO copy? (pls pos)
             .value = token_entry,
             .kind = token_kind,
         };
         try token_list.append(token);
-    }
-
-    var v_count: usize = 0;
-    var f_count: usize = 0;
-    var vt_count: usize = 0;
-    var vn_count: usize = 0;
-    var str_count: usize = 0;
-    for (token_list.items) |token| {
-        switch (token.kind) {
-            .v_sym => v_count += 1,
-            .f_sym => f_count += 1,
-            .vt_sym => vt_count += 1,
-            .vn_sym => vn_count += 1,
-            .string => str_count += 1,
-            else => {},
-        }
     }
 
     // std.debug.print(
@@ -122,10 +93,20 @@ fn parseObj(allocator: std.mem.Allocator) !void {
     //     f_count,
     //     str_count,
     // });
+    return token_list.toOwnedSlice();
+}
+// parse
+pub fn parseLexicalTokenList(allocator: std.mem.Allocator, token_list: []WavefrontObjLexicalToken) !WavefrontObj {
+    var v_count: usize = 0;
+    for (token_list) |token| {
+        switch (token.kind) {
+            .v_sym => v_count += 1,
+            else => {},
+        }
+    }
 
-    // parse
-    var v_list = FixedBufferList([3]f32).init(try arena.allocator().alloc([3]f32, v_count));
-    var token_reader = SliceReader(WavefrontObj.Token){ .items = token_list.items };
+    var v_list = FixedBufferList([3]f32).init(try allocator.alloc([3]f32, v_count));
+    var token_reader = SliceReader(WavefrontObjLexicalToken){ .items = token_list };
     while (token_reader.readOrErr()) |token| {
         switch (token.kind) {
             .v_sym => {
@@ -140,13 +121,33 @@ fn parseObj(allocator: std.mem.Allocator) !void {
                     return error.UnexpectEndOfStream;
                 }
             },
+            .f_sym => {},
             else => {},
         }
     } else |_| {}
-
-    // std.debug.print("vertexes {}\n", .{v_list});
+    return WavefrontObj{
+        .vertex_list = v_list.items,
+    };
 }
 
 test {
-    parseObj(std.testing.allocator) catch unreachable;
+    const allocator = std.testing.allocator;
+
+    const path = try std.fs.cwd().realpathAlloc(allocator, "assets/models/Wolfen_2/Wolfen2.obj");
+    defer allocator.free(path);
+    const file = try std.fs.openFileAbsolute(path, .{});
+    defer file.close();
+    const reader = file.reader();
+    const file_buffer = try reader.readAllAlloc(allocator, gigabytes(1));
+    defer allocator.free(file_buffer);
+    const token_list = try lexBuffer(allocator, file_buffer);
+    defer allocator.free(token_list);
+    var obj = try parseLexicalTokenList(allocator, token_list);
+    defer obj.free(allocator);
+
+    // try std.testing.expectEqual(obj.vertex_list[0], [3]f32{ 1, 2.5, -3 });
+    try std.testing.expectEqual(obj.vertex_list.len, 216);
+
+    try std.testing.expectEqual(obj.vertex_list[0], .{ 0.000000, 0.000000, 0.494990 });
+    try std.testing.expectEqual(obj.vertex_list[1], .{ 0.044985, 0.164962, -0.059962 });
 }
